86c188c7f4f33456f181ecd0a7cc3ee957778e04
Neil Smith <s3nt1n3lz18@gmail.com>
2021-11-19 14:19:57 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'track-by-example';
+  collection = [];
+
+  constructor() {
+    this.collection = [{id: 1}, {id: 2}, {id: 3}];
+  }
+  
+  addItem() {
+    this.collection.push({ id: this.collection.length + 1});
+  }
+  
+  trackByFn(index, item) {
+    return index; // or item.id
+  }
 }
